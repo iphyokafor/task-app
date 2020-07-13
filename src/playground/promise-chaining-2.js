c409f6fa08fe8b170fff43fc3789829a18217946
iphyokafor <sandraifeoma22@gmail.com>
2020-07-13 10:19:54 +0100

    learn promise syntax

--- src/playground/promise-chaining-2.js
+++ src/playground/promise-chaining-2.js
@@ -1,13 +1,27 @@
 require('../db/mongoose');
 const Task = require('../models/task');
 
-Task.findByIdAndDelete('5f0bd3210588de369ccfacc0')
-	.then((task) => {
-		console.log(task);
-		return Task.countDocuments({ completed: false });
-	})
-	.then((result) => {
-		console.log(result);
+// Task.findByIdAndDelete('5f0bd3210588de369ccfacc0')
+// 	.then((task) => {
+// 		console.log(task);
+// 		return Task.countDocuments({ completed: false });
+// 	})
+// 	.then((result) => {
+// 		console.log(result);
+// 	})
+// 	.catch((e) => {
+// 		console.log(e);
+// 	});
+
+const deleteTaskAndCount = async (id) => {
+	const task = await Task.findByIdAndDelete(id);
+	const count = await Task.countDocuments({ completed: false });
+	return count;
+};
+
+deleteTaskAndCount('5f0bd2ff0588de369ccfacbf')
+	.then((count) => {
+		console.log(count);
 	})
 	.catch((e) => {
 		console.log(e);
--- src/playground/promise-chaining.js
+++ src/playground/promise-chaining.js
@@ -1,13 +1,27 @@
 require('../db/mongoose');
 const User = require('../models/user');
 
-User.findByIdAndUpdate('5f0bca6cb4215e1a50440bf2', { age: 1 })
-	.then((user) => {
-		console.log(user);
-		return User.countDocuments({ age: 1 });
-	})
-	.then((result) => {
-		console.log(result);
+// User.findByIdAndUpdate('5f0bca6cb4215e1a50440bf2', { age: 1 })
+// 	.then((user) => {
+// 		console.log(user);
+// 		return User.countDocuments({ age: 1 });
+// 	})
+// 	.then((result) => {
+// 		console.log(result);
+// 	})
+// 	.catch((e) => {
+// 		console.log(e);
+// 	});
+
+const updateAgeAndCount = async (id, age) => {
+	const user = await User.findByIdAndUpdate(id, { age });
+	const count = await User.countDocuments({ age });
+	return count;
+};
+
+updateAgeAndCount('5f0bca6cb4215e1a50440bf2', 2)
+	.then((count) => {
+		console.log(count);
 	})
 	.catch((e) => {
 		console.log(e);
