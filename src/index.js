c4cd5756b7c63e98a8cc48db6a2a591884bb94ae
iphyokafor <sandraifeoma22@gmail.com>
2021-02-09 14:23:55 +0100

    add image upload endpoint

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
-const express = require('express');
+const express = require("express");
 
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
-require('./db/mongoose');
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+require("./db/mongoose");
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+// const multer = require("multer");
+// const upload = multer({
+//     dest: "avatars",
+//     limits: {
+//         fileSize: 1000000,
+//     },
+//     fileFilter(req, file, cb) {
+//         if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+//             return cb(new Error("Please upload only jpg, jpeg or png"));
+//         }
+
+//         cb(undefined, true);
+//     },
+// });
+
+// app.post(
+//     "/upload",
+//     upload.single("upload"),
+//     (req, res) => {
+//         res.send();
+//     },
+//     (error, req, res, next) => {
+//         res.status(400).send({ error: error.message });
+//     }
+// );
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
 app.listen(port, () => {
-	console.log('Server is up on port ' + port);
+    console.log("Server is up on port " + port);
 });
